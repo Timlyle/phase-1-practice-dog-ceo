3e027a92a20b823a0209e9c19dd8c710f5f75bf9
Timothy Lyle <itsfreezingouthere@yahoo.com>
2022-02-07 16:32:08 -0500

    commit tree aeec87671f470b700c9d29fe8f647270e49370cd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log('%c HI', 'color: firebrick')
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then (res => res.json())
+.then (dogs => console.log(dogs.message))
+.catch(error => console.error(error))
+
+function processDogImages(dogImages) {
+   const container = document.querySelector("#dog-image-container")
+   
+    dogImages.forEach(dog => {
+    const img = document.createElement('img')    
+    img.src = dog    
+    img.alt = "image of dog"    
+    container.appendChild('img')        
+})
+}
+
+fetch('https://dog.ceo/api/breeds/list/all')
+.then (res => res.json)
+.then(dogNames => console.log(dogNames))
+.catch(error => console.error(error))
